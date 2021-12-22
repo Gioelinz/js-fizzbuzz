15602ce7f1b9c57f4508fa3a4ea9ab2e84165257
Gioelinz <gioelerunza.gr@gmail.com>
2021-12-22 16:33:53 +0100

    fix fizzbuzz

--- js/script.js
+++ js/script.js
@@ -7,16 +7,13 @@ ma per i multipli di 3 stampi Fizz al posto del numero e per i multipli di 5 sta
 Per i numeri che sono sia multipli di 3 che di 5 stampi FizzBuzz.
 */
 
-
-
 //stampo i numeri da 1 a 10
+
 
 for (let i = 1; i <= 100; i++) {
     let result = '';
-    //condizioni 
     if (i % 3 === 0) result += 'Fizz';
+    if (i % 5 === 0) result += 'Buzz';
 
-    else if (i % 5 === 0) result += 'Buzz';
-
-    console.log("Risultato: ", result || i);
+    console.log(`il risultato è ${result || i}`);
 }
